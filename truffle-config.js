13457fd4dd9092144cc5320b6fb003e403ea9fc2
Hiệp Nguyễn Hoàng <81144804+hipngx@users.noreply.github.com>
2022-07-08 18:35:36 +0700

    update dotennv

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,9 @@
 require("babel-register");
 require("babel-polyfill");
+import "dotenv/config";
 const HDWalletProvider = require("truffle-hdwallet-provider");
-MNENOMIC =
-  "credit become rally life polar apology elevator enact now fury garbage dawn"; // Thay ABC bằng seed word của account bạn muốn dùng vào đây.
-INFURA_API_KEY = "341a7cd1860644a496c0e9bcefa956db"; // Thay 123 bằng API KEY của Infura vào đây
+MNENOMIC = process.env.MNENOMIC; // Thay ABC bằng seed word của account bạn muốn dùng vào đây.
+INFURA_API_KEY = process.env.INFURA_API_KEY; // Thay 123 bằng API KEY của Infura vào đây
 module.exports = {
   networks: {
     development: {
